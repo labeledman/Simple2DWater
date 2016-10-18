34c418eb03e97836ad3ef4059a767dfd1f719373
cvid <cvid@list.ru>
2016-10-18 22:35:06 +0300

    waves added

--- Assets/Scripts/Simple2DWater.cs
+++ Assets/Scripts/Simple2DWater.cs
@@ -5,6 +5,13 @@ using System.Collections.Generic;
 [RequireComponent(typeof(MeshFilter))]
 public class Simple2DWater : MonoBehaviour {
 
+	public int edgeCount = 32;
+
+	[Range(2,12)]
+	public int wavesCount = 4;
+
+	public float waveHeight = 1.0f;
+	
 	public float waterHeight = 5.0f;
 	public float waterWeight = 3.0f;
 
@@ -12,23 +19,35 @@ public class Simple2DWater : MonoBehaviour {
 	float[] yPositions;
 	Mesh waterMesh;
 
+	MeshFilter meshFilter;
+
+
+	public float speed = 5.0f;
+	private float paramT = 0.0f;
+
+	Vector3[] currentPositions;
+
 	void Start ()
 	{
+		meshFilter = GetComponent<MeshFilter>();
 		SpawnWaterMesh();
 	}
 
 
 	void SpawnWaterMesh()
 	{
-		int edgeCount = 5;
         int nodeCount = edgeCount + 1;
 
 		xPositions = new float[nodeCount];
         yPositions = new float[nodeCount];
 
+		currentPositions = new Vector3[nodeCount];
+
+
         for (int i = 0; i < nodeCount; i++)
         {
-			yPositions[i] = waterHeight;
+			float angle = 2.0f*(wavesCount - 1.0f)*(i*Mathf.PI)/(nodeCount);
+			yPositions[i] = waterHeight + waveHeight*Mathf.Cos(angle);
             xPositions[i] = -waterWeight/2.0f + waterWeight * i / edgeCount;
         }
 
@@ -81,6 +100,56 @@ public class Simple2DWater : MonoBehaviour {
 		waterMesh.uv = uvList.ToArray();
 		waterMesh.triangles = indexList.ToArray();
 
-		GetComponent<MeshFilter>().mesh = waterMesh;
+		meshFilter.mesh = waterMesh;
+	}
+
+	void Update()
+	{
+		AnimateWaves();
+	}
+
+	void AnimateWaves()
+	{
+		paramT += Time.deltaTime*speed;
+
+		if(paramT > 1.0f)
+		{
+			paramT = 0.0f;
+			float bufY = yPositions[0];
+			for (int i = 0; i < xPositions.Length-1; i++)
+			{
+				yPositions[i] = yPositions[i+1];
+			}
+
+			yPositions[yPositions.Length-1] = bufY;
+		}
+		else
+		{
+			for (int i = 0; i < xPositions.Length-1; i++)
+			{
+				currentPositions[i].y = Mathf.Lerp(yPositions[i], yPositions[i+1], paramT);
+			}
+			currentPositions[xPositions.Length-1].y = Mathf.Lerp(yPositions[yPositions.Length-1], yPositions[0], paramT);
+		}
+
+
+		List<Vector3> verticesList = new List<Vector3>();
+		for (int i = 0; i < edgeCount; i++)
+        {
+			Vector3[] Vertices = new Vector3[4];
+			Vertices[0] = new Vector3(xPositions[i], currentPositions[i].y, 0);
+			Vertices[1] = new Vector3(xPositions[i+1], currentPositions[i+1].y, 0);
+			Vertices[2] = new Vector3(xPositions[i], 0, 0);
+			Vertices[3] = new Vector3(xPositions[i+1], 0, 0);
+
+			verticesList.Add(Vertices[0]);
+			verticesList.Add(Vertices[1]);
+			verticesList.Add(Vertices[2]);
+			verticesList.Add(Vertices[3]);
+        }
+		waterMesh.vertices = verticesList.ToArray();
+
+
+		meshFilter.mesh = waterMesh;
 	}
 }
